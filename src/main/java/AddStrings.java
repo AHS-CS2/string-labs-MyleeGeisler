3e09b071bad227e6809ffd01fdf9985561a064b5
MYLEE GEISLER <228235@andrews.esc18.net>
2026-02-24 10:44:17 -0600

    First lab completed

--- src/main/java/AddStrings.java
+++ src/main/java/AddStrings.java
@@ -1,10 +1,10 @@
 //(c)  A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Mylee Geisler
+//Date - 2 - 20 - 26
+//Class - Computer Science I
+//Lab  - String Basics
 
 import static java.lang.System.*;
 
@@ -15,23 +15,40 @@ public class AddStrings
 
    public AddStrings()
    {
+    setStrings("hello", "world");
    }
 
    public AddStrings(String one, String two)
    {
+    setStrings(one, two);
    }
 
    public void setStrings(String one, String two)
    {
+      first = one;
+      last = two;
    }
 
- 	public void add( )
+   public String printFirst()
+   {
+      String result1 = "first :: " + first;
+      return result1;
+   }
+
+   public String printLast()
+   {
+      String result2 = "last :: " + last;
+      return result2;
+   }
+
+ 	public void add()
  	{
+      sum = first + " " + last;
 	}
 
  	public String toString()
  	{
- 		String output="";
+ 		String output= "sum :: " + sum;
  		return output;
 	}
 }
