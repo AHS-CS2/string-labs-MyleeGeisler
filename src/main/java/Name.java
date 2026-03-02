491a1b2f42f51a603435ac4d7db19bbe87b8ac8a
MYLEE GEISLER <228235@andrews.esc18.net>
2026-03-02 11:03:09 -0600

    Finished Lab 4, which is the String Name lab.

--- src/main/java/Name.java
+++ src/main/java/Name.java
@@ -1,41 +1,50 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Mylee Geisler
+//Date - 2 - 27 - 26
+//Class - Computer Science I
+//Lab  - String Name
 
 import static java.lang.System.*;
 
 public class Name
 {
 	private String name;
+	private String first;
+	private String last;
+	private int spaceIndex;
 
 	public Name()
 	{
+		setName("Timothy Smith");
 	}
 
 	public Name(String s)
 	{
+		setName(s);
 	}
 
    public void setName(String s)
    {
+		name = s;
+		spaceIndex = name.indexOf(" ");
    }
 
 	public String getFirst()
 	{
-		return "";
+		first = name.substring(0, spaceIndex);
+		return first;
 	}
 
 	public String getLast()
 	{
-		return "";
+		last = name.substring(spaceIndex + 1);
+		return last;
 	}
 
  	public String toString()
  	{
- 		return "";
+ 		return name + "\n\n";
 	}
 }
